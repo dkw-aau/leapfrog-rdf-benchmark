1a5c834ec2e1178fef31276b919aea59c6e5b010
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2023-06-30 10:57:10 +0200

    Filter runtimes according to Jena

--- results/print_results.py
+++ results/print_results.py
@@ -2,20 +2,19 @@ import os
 
 groups = ['bgps', 'empty', 'existence_check', 'optionals', 'specialized', 'modified_bgps']
 
-for group in groups:
-    print(group)
+# Returns map from query shape in group to Jena version to list of runtimes
+def get_results(group):
+    shapes = os.listdir(group)
+    shape_map = dict()
 
-    types = os.listdir(group)
-
-    for type in types:
-        print('\n' + type)
-
-        results_path = group + '/' + type + '/'
+    for shape in shapes:
+        shape_map[shape] = dict()
+        results_path = group + '/' + shape + '/'
         versions = os.listdir(results_path)
 
         for version in versions:
+            shape_map[shape][version] = list()
             first = True
-            print('\n' + version)
 
             with open(results_path + version, 'r') as file:
                 for line in file:
@@ -24,12 +23,60 @@ for group in groups:
                         continue
 
                     elif 'timeout' in line:
-                        continue
+                        shape_map[shape][version].append(-1)
 
-                    ms = 1000000
-                    time_ms = float(line.strip().split(',')[2]) / ms
+                    else:
+                        time_ms = float(line.strip().split(',')[2]) / 1000000	# Converted to ms
+                        shape_map[shape][version].append(time_ms)
 
-                    if time_ms >= 100:
-                        print(str(time_ms).replace('.', ','))
+    return shape_map
 
-    print('\n')
+# Returns None if everything is alright
+# Otherwise, it returns a tuple indicating which query shape contains the size mis-match
+def check_results_count(map):
+    for shape in map.keys():
+        size = len(map[shape][list(map[shape].keys())[0]])
+
+        for version in map[shape].keys():
+            if len(map[shape][version]) != size:
+                return shape
+
+    return None
+
+# Filter results according to runtime of a given Jena version
+def filter_runtimes(map, version, runtime):
+    for shape in map.keys():
+        positions = list()
+        i = 0
+
+        for time in map[shape][version]:
+            if time < runtime:
+                positions.append(i)
+
+            i += 1
+
+        positions.reverse()
+
+        for version in map[shape].keys():
+            for position in positions:
+                map[shape][version].pop(position)
+
+for group in groups:
+    map = get_results(group)
+    check = check_results_count(map)
+
+    if check != None:
+        print(check)
+        break
+
+    filter_runtimes(map, 'jena.csv', 100)
+    print(group)
+
+    for shape in map.keys():
+        print(shape)
+
+        for version in map[shape].keys():
+            print('\n' + version)
+
+            for time in map[shape][version]:
+                print(time)
